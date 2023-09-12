7723a8a353c94fd6341605e45c14c63534b4773c
戴文成 <1319105206@qq.com>
2023-09-12 18:58:06 +0800

    创建一个架子，并调通接口 1.[yudao-server] 1.1空壳项目，作为启动使用。其他模块会最为依赖引入，进行整合。 1.2调通接口 2.[yudao-dependencies] 2.1基础 bom 文件，管理整个项目的依赖版本 2.2管理springboot版本为2.7.13 3.[最外层pom.xml] 3.1作为其他所有模块的父pom，管理项目的版本号和依赖 3.2版主仲裁中使用[yudao-dependencies]，将[yudao-dependencies]的依赖传递给所有的子模块，进而统一管理所有模块的依赖

---
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/controller/TestController.java
@@ -0,0 +1,16 @@
+package cn.iocoder.yudao.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+
+    @GetMapping("/test")
+    public Object test() {
+        return "测试";
+    }
+
+}
