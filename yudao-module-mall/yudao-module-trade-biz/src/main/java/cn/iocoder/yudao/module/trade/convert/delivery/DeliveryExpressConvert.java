c28e3c4fd23e215d21977e00e01bad44e24e2b73
戴文成 <1319105206@qq.com>
2023-11-22 17:20:31 +0800

    配送管理-快递发货-快递公司

---
+++ yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/convert/delivery/DeliveryExpressConvert.java
@@ -0,0 +1,29 @@
+package cn.iocoder.yudao.module.trade.convert.delivery;
+
+import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.module.trade.controller.admin.delivery.vo.express.*;
+import cn.iocoder.yudao.module.trade.dal.dataobject.delivery.DeliveryExpressDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface DeliveryExpressConvert {
+
+    DeliveryExpressConvert INSTANCE = Mappers.getMapper(DeliveryExpressConvert.class);
+
+    DeliveryExpressDO convert(DeliveryExpressCreateReqVO bean);
+
+    DeliveryExpressDO convert(DeliveryExpressUpdateReqVO bean);
+
+    DeliveryExpressRespVO convert(DeliveryExpressDO bean);
+
+    PageResult<DeliveryExpressRespVO> convertPage(PageResult<DeliveryExpressDO> page);
+
+    List<DeliveryExpressExcelVO> convertList02(List<DeliveryExpressDO> list);
+
+    List<DeliveryExpressSimpleRespVO> convertList1(List<DeliveryExpressDO> list);
+
+
+}
