bedf6895cec19990af1060df500bf784f7dae600
戴文成 <1319105206@qq.com>
2023-11-23 17:54:30 +0800

    门店自提-门店管理

---
+++ yudao-module-mall/yudao-module-trade-biz/src/main/java/cn/iocoder/yudao/module/trade/convert/delivery/DeliveryPickUpStoreConvert.java
@@ -0,0 +1,32 @@
+package cn.iocoder.yudao.module.trade.convert.delivery;
+
+import cn.iocoder.yudao.framework.common.pojo.PageResult;
+import cn.iocoder.yudao.module.trade.controller.admin.delivery.vo.pickup.DeliveryPickUpStoreCreateReqVO;
+import cn.iocoder.yudao.module.trade.controller.admin.delivery.vo.pickup.DeliveryPickUpStoreRespVO;
+import cn.iocoder.yudao.module.trade.controller.admin.delivery.vo.pickup.DeliveryPickUpStoreSimpleRespVO;
+import cn.iocoder.yudao.module.trade.controller.admin.delivery.vo.pickup.DeliveryPickUpStoreUpdateReqVO;
+import cn.iocoder.yudao.module.trade.dal.dataobject.delivery.DeliveryPickUpStoreDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface DeliveryPickUpStoreConvert {
+
+    DeliveryPickUpStoreConvert INSTANCE = Mappers.getMapper(DeliveryPickUpStoreConvert.class);
+
+    DeliveryPickUpStoreDO convert(DeliveryPickUpStoreCreateReqVO bean);
+
+    DeliveryPickUpStoreDO convert(DeliveryPickUpStoreUpdateReqVO bean);
+
+    DeliveryPickUpStoreRespVO convert(DeliveryPickUpStoreDO bean);
+
+    List<DeliveryPickUpStoreRespVO> convertList(List<DeliveryPickUpStoreDO> list);
+
+    PageResult<DeliveryPickUpStoreRespVO> convertPage(PageResult<DeliveryPickUpStoreDO> page);
+
+    List<DeliveryPickUpStoreSimpleRespVO> convertList1(List<DeliveryPickUpStoreDO> list);
+
+
+}
